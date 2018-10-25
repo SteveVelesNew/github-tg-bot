220a502dc2a68f005847a64164c8158a24860dc5
SteveVelesNew <holovchenkostepok@gmail.com>
2018-10-25 18:31:49 +0300

    instal

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ require('http').createServer().listen(process.env.PORT || 5000).on('request', fu
     res.end('')
 })﻿
 //============================//
+//============not Idline - тихий час================//
+
+var http = require('http'); //importing http
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'your_app_name.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+//============not Idline - тихий час================//
+
 
 // екземпляр класу бота
 const bot = new TelegramBot(TOKEN, {
