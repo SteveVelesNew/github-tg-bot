69718a887d42291ef67d68a37a6b6ea1554997a2
SteveVelesNew <holovchenkostepok@gmail.com>
2018-10-29 10:22:04 +0300

    instal

--- index.js
+++ index.js
@@ -9,32 +9,10 @@ require('http').createServer().listen(process.env.PORT || 5000).on('request', fu
 })﻿
 //============================//
 //============not Idline - тихий час================//
-
-var http = require('http'); //importing http
-
-function startKeepAlive() {
-    setInterval(function() {
-        var options = {
-            host: 'your_app_name.herokuapp.com',
-            port: 80,
-            path: '/'
-        };
-        http.get(options, function(res) {
-            res.on('data', function(chunk) {
-                try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU RESPONSE: " + chunk);
-                } catch (err) {
-                    console.log(err.message);
-                }
-            });
-        }).on('error', function(err) {
-            console.log("Error: " + err.message);
-        });
-    }, 20 * 60 * 1000); // load every 20 minutes
-}
-
-startKeepAlive();
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 //============not Idline - тихий час================//
 
 
